f255fc5ca1e6e1708fe3751a21bb1f0d893be786
Joao-Marcelo01 <162449081+Joao-Marcelo01@users.noreply.github.com>
2025-02-24 19:18:13 -0300

    Lista de Exercícios lista e pilha

---
+++ lista/src/lista/TesteLista.java
@@ -0,0 +1,15 @@
+package lista;
+
+public class TesteLista {
+    public static void main(String[] args) {
+        ListaArray lista = new ListaArray(5);
+        lista.adicionar(10);
+        lista.adicionar(20);
+        lista.adicionar(30);
+        lista.imprimirLista(); 
+        lista.adicionarNaPosicao(15, 1); 
+        lista.imprimirLista();  
+        lista.remover(2);  
+        lista.imprimirLista();  
+    }
+}
